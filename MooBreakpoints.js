b35a01772e61d54a4cfc912d911301796398d8bd
Kyle <halldirector@gmail.com>
2012-07-22 13:40:27 -0400

    major rewrite/refactor
    further testing showed bugs/dumb code

--- MooBreakpoints.js
+++ MooBreakpoints.js
@@ -1,32 +1,60 @@
+/*
+---
+description: 
+    - provides events when the window passes width thresholds
+    - can be used as a polyfill for media query support
+    - can be used as a complement to media queries
+        (i.e. javascript that is only needed with media queries)
+    
+authors:
+    - Kyle J. Davis (http://stockholmux.com)
+
+license:
+    - MIT-style license
+
+provides:
+    - MooBreakpoints
+*/
+
 var MooBreakpoints = new Class({
-    options: { breakPoints : [320, 480, 768, 1024] },
+    options: { breakPoints : [320, 480, 768, 1024],
+               delay: 250
+            },
     Implements: Options,
     initialize: function (options) {
         'use strict';
         var resizeDelay,
             currentBreakPoint,
             that = this,
-            resizeFirer = function () {
-                var i,
+            resizeFirer;
+            
+        this.setOptions(options);
+        
+        resizeFirer = function () {
+                var i = 0,
                     windowSize = window.getSize(),
                     fired = false;
                 
-                
-                for (i = that.options.breakPoints.length - 1; i > -1; i -= 1) {
-                    if ((windowSize.x >= that.options.breakPoints[i]) && (!fired)) {
-                        fired = true;
+                while((i <= (that.options.breakPoints.length -1)) && (!fired)) {
+                    if ((windowSize.x <= that.options.breakPoints[i])  && (that.options.breakPoints[i] !== currentBreakPoint)) {
+                        
                         if (currentBreakPoint !== that.options.breakPoints[i]) {
-                            window.fireEvent('onWidthEnter' + that.options.breakPoints[i], windowSize);
-                            window.fireEvent('onWidthLeave' + currentBreakPoint, windowSize);
+                            window.fireEvent('onWidthLeave' + currentBreakPoint, [currentBreakPoint, windowSize]);
                         }
-                        currentBreakPoint = that.options.breakPoints[i];
+        
+                        window.fireEvent('onWidthEnter' + that.options.breakPoints[i], [that.options.breakPoints[i], windowSize]);
+                        
+                        currentBreakPoint = that.options.breakPoints[i]
+                        fired = true;
                     }
+                    
+                    i += 1;
                 }
             };
-        this.setOptions(options); 
+        
         window.addEvent('resize', function () {
             clearTimeout(resizeDelay);
-            resizeDelay = resizeFirer.delay(250);
+            resizeDelay = resizeFirer.delay(that.options.delay);
         });
     }
 });
